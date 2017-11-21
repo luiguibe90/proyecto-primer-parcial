4d517dad0b26fcb69438c56c380a82a8a0e3e913
luiguibe90 <32905666+luiguibe90@users.noreply.github.com>
2017-11-21 08:51:14 -0500

    V2
    ARREGLO ESTRUCTURA DE PROGRAMA GLOBAL

--- Proyecto_1er_Parcial_boletos_1_bus/main.cpp
+++ Proyecto_1er_Parcial_boletos_1_bus/main.cpp
@@ -44,6 +44,7 @@ void gotoxy(int,int);
 void color(int,int);
 int menu();
 int menuSN();
+void inicio ();
 int ObtenerEnteroPositivo();
 int insElemListaVacia(ListaDoble &);
 void insElemInicio(ListaDoble &);
@@ -70,9 +71,16 @@ int ObtenerEnteroPositivo(){
     return entero=atoi(cadenaDelEntero);
 }
 
-int insElemListaVacia(ListaDoble &lista)
+void buscarCliente(ListaDoble &lista){
+
+}
+
+void insertarDatosClientePosicion(ListaDoble &lista){
+
+}
+void insDatosCliente(ListaDoble &lista)
 {
-    ListaDoble nuevoElemento;
+    ListaDoble nuevoElemento,nuevoAux;
     nuevoElemento=new(Nodo);
     system("cls");
     printf("\t----------------------------\n");
@@ -86,13 +94,20 @@ int insElemListaVacia(ListaDoble &lista)
         nuevoElemento->siguiente=nuevoElemento->anterior=NULL;
         lista=nuevoElemento;
         printf("\n\nPrimer numero ingresado!\n\n");
-    }else{
-        printf("Ya ingreso el primer elemento!\n\n");
     }
-    return 0;
-}
+    else{
 
-
+         nuevoElemento->datoEntero=ObtenerEnteroPositivo();
+        nuevoAux=lista;
+        while(nuevoAux->siguiente!=NULL){
+            nuevoAux=nuevoAux->siguiente;
+        }
+        nuevoElemento->siguiente=nuevoAux->siguiente;
+        nuevoAux->siguiente=nuevoElemento;
+        nuevoElemento->anterior=nuevoAux;
+        printf("\n\nDato ingresado!\n\n");
+    }
+    }
 void insElemInicio(ListaDoble &lista)
 {
     ListaDoble nuevoElemento,nuevoAux;
@@ -266,7 +281,7 @@ void insElemDespues(ListaDoble &lista)
     }
 
 }
-void eliminarDato(ListaDoble &lista,int valor)
+void cancelarBoleto(ListaDoble &lista,int valor)
 {
     ListaDoble aux=new(Nodo);
     ListaDoble anterior=new(Nodo);
@@ -350,7 +365,7 @@ void liberarMemoria(ListaDoble &lista){
 int main()
 {
     ListaDoble lista=NULL;
-
+    inicio();
     color (fondo,texto);
 	int opcion,numero=0,control=0;
 	do{
@@ -358,29 +373,11 @@ int main()
 		system("cls");
 		switch (opcion){
             case 1:
-                insElemListaVacia(lista);
+                insDatosCliente(lista);
                 control=1;
                 system("pause");
                 break;
             case 2:
-                insElemInicio(lista);
-                system("pause");
-                break;
-            case 3:
-                insElemFin(lista);
-                system("pause");
-                break;
-            case 4:
-                insElemAntes(lista);
-                system("pause");
-
-                break;
-            case 5:
-                insElemDespues(lista);
-                system("pause");
-
-                break;
-            case 6:
                 if(control==0)
                 {
                     printf("Primero Debe Ingresar Datos a la lista\n");
@@ -392,40 +389,43 @@ int main()
                         printf("\nIngrese Dato a Buscar: \n");
                         numero=ObtenerEnteroPositivo();
                     }while(numero<0);
-                    eliminarDato(lista,numero);
+                    cancelarBoleto(lista,numero);
                 }
+                system("pause");
+                break;
+            case 3:
+                buscarCliente(lista);
+                system("pause");
+                break;
+            case 4:
+                insertarDatosClientePosicion(lista);
                 system("pause");
 
                 break;
-            case 7:
+            case 5:
+                mostrarDatos(lista);
+                system("pause");
 
+                break;
+            case 6:
+
+               // ayuda();
+                system("AyudaHash.chm");
+                system("pause");
+                break;
+            case 7:
+                //about();
+                system("20161224_133008.jpg");
                 system("pause");
 
                 break;
             case 8:
-                mostrarDatos(lista);
-                system("pause");
-                break;
-            case 9:
-                liberarMemoria(lista);
-                system("pause");
-                break;
-            case 10:
-                 system("AyudaHash.chm");
-                //system("pause");
-                break;
-            case 11:
-                system("20161224_133008.jpg");
-                break;
 
+                break;
         }
-
-
-	}while(opcion!=11);
-
+	}while(opcion!=8);
 return 0;
 }
-
 void gotoxy(int x,int y){
       HANDLE hcon;
       hcon = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -438,15 +438,13 @@ void color(int a,int b){
     int color= (a*16)+b;
     SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE),color);
 }
-
 int menu(){
 
     const char *titulo="\t\t\tLISTAS DOBLES";
 	const char *opciones[]={/*1*/"INGRESAR DATOS DE CLIENTE",/*2*/"CANCELACION BOLETO(ELIMINAR)"
-                            ,/*3*/"INGRESAR DATO ENTERO POSITIVO AL FINAL DE LA LISTA",/*4*/"INGRESAR DATO ENTERO POSITIVO ANTES DE UNA POSICION",
-                            /*5*/"INGRESAR DATO ENTERO POSITIVO DESPUES DE UNA POSICION",/*6*/"ELIMINAR DATO DE LISTA",
-                            /*7*/"NUMEROS PRIMOS DE LISTA", /*8*/"MOSTRAR ELEMENTOS DE LISTA",/*9*/"SALIR",/*10*/"ayuda",/*11*/"foto"};
-    int opcionSeleccionada = 1,numerodeopciones=11;
+                            ,/*3*/"BUSCAR CLIENTE",/*4*/"INGRESAR DATOS CLIENTE EN DETERMINADO ASIENTO",
+                            /*5*/"IMPRIMIR DATOS",/*6*/"AYUDA", /*7*/"ABOUT",/*8*/"SALIR"};
+    int opcionSeleccionada = 1,numerodeopciones=8;
     int tecla;
     bool repite=true;
     int i=0;
@@ -547,4 +545,198 @@ int menuSN(){
         }
     }while(repite);
     return opcionSeleccionada;
+}
+void inicio (){
+    /* LIBRERIA COLORES
+        Colores :
+        BLACK			0 Negro
+        BLUE			1 Azul
+        GREEN			2 Verde
+        CYAN			3 Cían
+        RED				4 Rojo
+        MAGENTA			5 Magenta
+        BROWN			6 Marrón
+        LIGHTGRAY		7 Gris Claro
+        DARKGRAY		8 Gris Oscuro
+        LIGHTBLUE		9 Azul Claro
+        LIGHTGREEN		A Verde Claro
+        LIGHTCYAN		B Cían Claro
+        LIGHTRED		C Rojo Claro
+        LIGHTMAGENTA	D Magenta Claro
+        YELLOW			E Amarillo
+        WHITE			F Blanco
+        */
+    int tesp=15,x=3,y=4;
+    char caracter[130];
+    system("color 0A");//color fondo-letra
+    FILE *INICIO;
+    INICIO=fopen("INICIO.txt","r");
+    //barra superior y barra izquierda
+         for (int i=1;i<117;i++) //horizontal
+        {
+            gotoxy(i,1);printf("%c",223);
+        }
+        for (int i=1;i<17;i++) //vertical
+        {
+            gotoxy(1,i);printf("%c",219);
+        }
+    //barra inferior y barra derecha
+          for (int i=1;i<17;i++) //horizontal
+        {
+            gotoxy(117,i);printf("%c",219);
+        }
+        for (int i=1;i<=117;i++) //barra inferior horizonatal
+        {
+            gotoxy(i,17);printf("%c",223);
+        }
+     // printf ("\n");
+        while (!feof(INICIO))
+        {
+            fgets(caracter,130,INICIO);
+            gotoxy(x,y);cout<<caracter;
+            Sleep(125);
+            y++;
+        }
+        printf("\n\n\n");
+        //system("pause");
+        Sleep(3000);
+        Beep(330,200);
+        system("cls");
+    system("color 0A");
+    //letras espe
+    //E
+
+        gotoxy(43,10);printf("%c",219);Sleep(tesp);
+        gotoxy(43,11);printf("%c",219);Sleep(tesp);
+        gotoxy(43,12);printf("%c",219);Sleep(tesp);
+        gotoxy(43,13);printf("%c",219);Sleep(tesp);
+        gotoxy(43,14);printf("%c",219);Sleep(tesp);
+        gotoxy(43,15);printf("%c",219);Sleep(tesp);
+        //gotoxy(43,16);printf("%c",219);Sleep(tesp);
+
+        gotoxy(44,10);printf("%c",223);Sleep(tesp);
+        gotoxy(45,10);printf("%c",223);Sleep(tesp);
+        gotoxy(46,10);printf("%c",223);Sleep(tesp);
+        gotoxy(47,10);printf("%c",223);Sleep(tesp);
+
+
+        gotoxy(44,13);printf("%c",223);Sleep(tesp);
+        gotoxy(45,13);printf("%c",223);Sleep(tesp);
+        gotoxy(46,13);printf("%c",223);Sleep(tesp);
+        gotoxy(47,13);printf("%c",223);Sleep(tesp);
+
+
+        gotoxy(43,16);printf("%c",223);Sleep(tesp);
+        gotoxy(44,16);printf("%c",223);Sleep(tesp);
+        gotoxy(45,16);printf("%c",223);Sleep(tesp);
+        gotoxy(46,16);printf("%c",223);Sleep(tesp);
+        gotoxy(47,16);printf("%c",223);Sleep(tesp);
+    Beep(800,300);
+        //S
+        gotoxy(50,10);printf("%c",219);Sleep(tesp);
+        gotoxy(50,11);printf("%c",219);Sleep(tesp);
+        gotoxy(50,12);printf("%c",219);Sleep(tesp);
+        //gotoxy(50,13);printf("%c",219);Sleep(tesp);
+        gotoxy(54,13);printf("%c",219);Sleep(tesp);
+        gotoxy(54,14);printf("%c",219);Sleep(tesp);
+        gotoxy(54,15);printf("%c",219);Sleep(tesp);
+
+        gotoxy(51,10);printf("%c",223);Sleep(tesp);
+        gotoxy(52,10);printf("%c",223);Sleep(tesp);
+        gotoxy(53,10);printf("%c",223);Sleep(tesp);
+        gotoxy(54,10);printf("%c",223);Sleep(tesp);
+
+        gotoxy(50,13);printf("%c",223);Sleep(tesp);
+        gotoxy(51,13);printf("%c",223);Sleep(tesp);
+        gotoxy(52,13);printf("%c",223);Sleep(tesp);
+        gotoxy(52,13);printf("%c",223);Sleep(tesp);
+        gotoxy(53,13);printf("%c",223);Sleep(tesp);
+
+        gotoxy(50,16);printf("%c",223);Sleep(tesp);
+        gotoxy(51,16);printf("%c",223);Sleep(tesp);
+        gotoxy(52,16);printf("%c",223);Sleep(tesp);
+        gotoxy(53,16);printf("%c",223);Sleep(tesp);
+        gotoxy(54,16);printf("%c",223);Sleep(tesp);
+    Beep(800,300);
+
+        //P
+        gotoxy(57,10);printf("%c",219);Sleep(tesp);
+        gotoxy(57,11);printf("%c",219);Sleep(tesp);
+        gotoxy(57,12);printf("%c",219);Sleep(tesp);
+        gotoxy(57,13);printf("%c",219);Sleep(tesp);
+        gotoxy(57,14);printf("%c",219);Sleep(tesp);
+        gotoxy(57,15);printf("%c",219);Sleep(tesp);
+        gotoxy(57,16);printf("%c",219);Sleep(tesp);
+
+        gotoxy(58,10);printf("%c",223);Sleep(tesp);
+        gotoxy(59,10);printf("%c",223);Sleep(tesp);
+        gotoxy(60,10);printf("%c",223);Sleep(tesp);
+        gotoxy(61,10);printf("%c",223);Sleep(tesp);
+
+
+        gotoxy(61,10);printf("%c",219);Sleep(tesp);
+        gotoxy(61,11);printf("%c",219);Sleep(tesp);
+        gotoxy(61,12);printf("%c",219);Sleep(tesp);
+        //gotoxy(61,13);printf("%c",219);Sleep(tesp);
+
+
+        gotoxy(61,13);printf("%c",223);Sleep(tesp);
+        gotoxy(60,13);printf("%c",223);Sleep(tesp);
+        gotoxy(59,13);printf("%c",223);Sleep(tesp);
+        gotoxy(58,13);printf("%c",223);Sleep(tesp);
+    Beep(800,300);
+
+        //E
+        gotoxy(64,10);printf("%c",219);Sleep(tesp);
+        gotoxy(64,11);printf("%c",219);Sleep(tesp);
+        gotoxy(64,12);printf("%c",219);Sleep(tesp);
+        gotoxy(64,13);printf("%c",219);Sleep(tesp);
+        gotoxy(64,14);printf("%c",219);Sleep(tesp);
+        gotoxy(64,15);printf("%c",219);Sleep(tesp);
+        //gotoxy(64,16);printf("%c",219);Sleep(tesp);
+
+        gotoxy(65,10);printf("%c",223);Sleep(tesp);
+        gotoxy(66,10);printf("%c",223);Sleep(tesp);
+        gotoxy(67,10);printf("%c",223);Sleep(tesp);
+        gotoxy(68,10);printf("%c",223);Sleep(tesp);
+
+
+        gotoxy(65,13);printf("%c",223);Sleep(tesp);
+        gotoxy(66,13);printf("%c",223);Sleep(tesp);
+        gotoxy(67,13);printf("%c",223);Sleep(tesp);
+        gotoxy(68,13);printf("%c",223);Sleep(tesp);
+
+
+        gotoxy(64,16);printf("%c",223);Sleep(tesp);
+        gotoxy(65,16);printf("%c",223);Sleep(tesp);
+        gotoxy(66,16);printf("%c",223);Sleep(tesp);
+        gotoxy(67,16);printf("%c",223);Sleep(tesp);
+        gotoxy(68,16);printf("%c",223);Sleep(tesp);
+    Beep(800,300);
+    gotoxy(41,25);printf("VENTA DE BOLETOS DE UN BUS\n");
+    //barra cargando
+
+    gotoxy(40,29);//1er numero mueve hacia izq o der 2do arriba abajo
+
+    printf("\tCARGANDO...\n");
+    for(int i=20;i<90;i++)//80 marca el tamanio
+    {
+        gotoxy(i,30);//1er numero mueve hacia izq o der 2do arriba abajo
+        printf("%c",219);//valor de caracter a imprimir para la barra mediante asccii
+        for(int x=50;x<90;x++)
+        {//los espacios que va a recorre la barra en la pantalla
+            for(int y=1;y<20;y++)
+            {//determina el tiempo de movimiento de la barra
+                gotoxy(y+180,34);
+            }
+        }
+    }
+    Beep(262,150);
+    Beep(262,150);
+    Beep(262,150);
+    printf("\n\n");
+    Sleep(1000);
+    //system("pause");
+    system("cls");
+    Beep(330,200);
 }
