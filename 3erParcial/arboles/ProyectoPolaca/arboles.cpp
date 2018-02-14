06defd1caba933462a93c148d86a46cced62ec13
luiguibe90 <32905666+luiguibe90@users.noreply.github.com>
2018-02-13 19:42:28 -0500

    P3 - V4
    aumento e instalacion de libreria allegro

--- 3erParcial/arboles/ProyectoPolaca/arboles.cpp
+++ 3erParcial/arboles/ProyectoPolaca/arboles.cpp
@@ -30,6 +30,8 @@
 #define TECLA_ARRIBA 72
 #define TECLA_ABAJO 80
 #define ENTER 13
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
 //2-6-7-9
 using namespace std;
 
@@ -70,12 +72,25 @@ void about(){
 void agente(){
 	system("WinAppManejoMSAgente.exe");
 }
+
+void allegro() {
+	al_init();
+	al_init_font_addon();
+	ALLEGRO_DISPLAY* display = al_create_display(800, 600);
+	ALLEGRO_FONT* font = al_create_builtin_font();
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+	al_draw_text(font, al_map_rgb(255, 255, 255), 400, 300, ALLEGRO_ALIGN_CENTER, "Welcome to Allegro!");
+	al_flip_display();
+	al_rest(5.0);
+}
 int main(){
 	color(fondo, texto);
 	//system("color 0A");
-	inicio();
+	//inicio();
+	//allegro();
 	menuMouse();
 	//system("color 0A");
+	
 	return 0;
 }
 void guardarAg(string expresion, char *archivo) {
